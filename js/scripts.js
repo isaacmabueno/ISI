0e69e40e0f1038bfd58989ed68de2e944066d466
isaacmabueno <isaacmabueno@gmail.com>
2018-07-15 21:12:14 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+jQuery(function ($) {
+    $(document).ready(function() {
+        $(".call-now").click(function() {
+            $(this).css("background", "#4a4a4a").text('504.347.5600');
+        });
+        $(".one a").hover(
+            function() {
+                $('.one img').attr("src", "/wp-content/uploads/2018/05/non-destructive-testing-icon.png");
+            },
+            function() {
+                $('.one img').attr("src", "/wp-content/uploads/2018/05/non-desctuctive-testing-hover.png");
+            }
+            );
+    });
+});
